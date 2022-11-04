be17b2f08aaffa69f8f98619b1a8c4ff27ec0636
Yang-9987 <1013404703@qq.com>
2022-11-05 00:25:41 +0800

    Initial Commit

---
+++ Life/LifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeApp.swift
+//  Life
+//
+//  Created by Yang Jianqi on 2022/11/5.
+//
+
+import SwiftUI
+
+@main
+struct LifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
